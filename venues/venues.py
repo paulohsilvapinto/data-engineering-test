c567051f266550d80872cf7dc6039a60187227ea
Paulo Henrique Silva Pinto <paulohsilvapinto@gmail.com>
2020-10-03 11:11:44 -0300

    Linting and normalization improvements

--- venues/venues.py
+++ venues/venues.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 from typing import List, Dict, TypeVar, Any
 
 T = TypeVar('T', str, List[str])
@@ -17,14 +18,17 @@ def _get_json_from_url(url: str) -> List[Dict[str, T]]:
 
 
 def _normalize_string(x: str) -> str:
-    return x.lower().strip()
+    # Removes unwanted spaces
+    # Converts strings to Title Case
+    return re.sub(r'\s+', ' ', x.strip()).title()
 
 
 # Converts user name to dict key and normalizes user preferences
-def parse_json_users(users_response: List[Dict[str, T]]) -> Dict[str, Dict[str, T]]:
+def parse_json_users(
+        users_response: List[Dict[str, T]]) -> Dict[str, Dict[str, T]]:
     users = {}
     for user in users_response:
-        users[user['name']] = {
+        users[_normalize_string(user['name'])] = {
             'wont_eat': [_normalize_string(food) for food in user['wont_eat']],
             'drinks': [_normalize_string(drink) for drink in user['drinks']]
         }
@@ -32,10 +36,11 @@ def parse_json_users(users_response: List[Dict[str, T]]) -> Dict[str, Dict[str, 
 
 
 # Converts venues name to dict key and normalizes venues menu
-def parse_json_venues(venues_response: List[Dict[str, T]]) -> Dict[str, Dict[str, T]]:
+def parse_json_venues(
+        venues_response: List[Dict[str, T]]) -> Dict[str, Dict[str, T]]:
     venues = {}
     for venue in venues_response:
-        venues[venue['name']] = {
+        venues[_normalize_string(venue['name'])] = {
             'food': [_normalize_string(food) for food in venue['food']],
             'drinks': [_normalize_string(drink) for drink in venue['drinks']]
         }
@@ -43,13 +48,15 @@ def parse_json_venues(venues_response: List[Dict[str, T]]) -> Dict[str, Dict[str
 
 
 # Selects only preferences of the selected users
-def filter_users(user_list: List[str], users: Dict[str, Dict[str, T]]) -> Dict[str, Dict[str, T]]:
+def filter_users(user_list: List[str],
+                 users: Dict[str, Dict[str, T]]) -> Dict[str, Dict[str, T]]:
     filtered_users = {}
     for user in user_list:
+        user = _normalize_string(user)
         try:
             filtered_users[user] = users[user]
         except KeyError as e:
-            print((f'Preferences of user {user} are not defined.'))
+            print((f'Preferences of user {user} were not found.'))
             raise e
 
     return filtered_users
@@ -61,22 +68,26 @@ def check_user_list(user_list: List[str]) -> List[str]:
     elif isinstance(user_list, str):
         return list(user_list)
     else:
-        raise TypeError('A user list containing user names was expected as input.')
+        raise TypeError(
+            'A user list containing user names was expected as input.')
 
 
-def _is_valid_food_menu(food_menu: List[str], food_restrictions: List[str]) -> bool:
+def _is_valid_food_menu(food_menu: List[str],
+                        food_restrictions: List[str]) -> bool:
     if set(food_menu).difference(set(food_restrictions)):
         return True
     return False
 
 
-def _is_valid_drink_menu(drink_menu: List[str], drinks_approved: List[str]) -> bool:
+def _is_valid_drink_menu(drink_menu: List[str],
+                         drinks_approved: List[str]) -> bool:
     if set(drink_menu).intersection(set(drinks_approved)):
         return True
     return False
 
 
-def check_venues(users: Dict[str, Dict[str, T]], venues: Dict[str, Dict[str, T]]) -> Dict[str, Any]:
+def check_venues(users: Dict[str, Dict[str, T]],
+                 venues: Dict[str, Dict[str, T]]) -> Dict[str, Any]:
     places_to_visit = []
     places_to_avoid = []
 
@@ -112,9 +123,10 @@ def check_venues(users: Dict[str, Dict[str, T]], venues: Dict[str, Dict[str, T]]
     }
 
 
-def get_compatible_venues(user_list: List[str],
-                          users_url: str = DEFAULT_USERS_URL,
-                          venues_url: str = DEFAULT_VENUES_URL) -> Dict[str, Any]:
+def get_compatible_venues(
+        user_list: List[str],
+        users_url: str = DEFAULT_USERS_URL,
+        venues_url: str = DEFAULT_VENUES_URL) -> Dict[str, Any]:
     user_list = check_user_list(user_list)
     users = parse_json_users(_get_json_from_url(users_url))
     venues = parse_json_venues(_get_json_from_url(venues_url))
